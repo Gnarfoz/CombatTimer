61df8b540494619a2c7dba64648d7a9d08b919b9
Gnarfoz <gnarfoz@gmail.com>
2010-10-04 00:48:14 +0200

    add deDE

--- CombatTimer.lua
+++ CombatTimer.lua
@@ -1,14 +1,21 @@
 local CombatTimerFrame = CreateFrame("Frame")
 local LastTime = GetTime()
 local SeparateFrame = nil
+local text
+
+if GetLocale() == "deDE" then
+	text = "Kampfdauer:"
+else
+	text = "Fight duration:"
+end
 
 local function SpitItOut(self, event, addon)
 	if event == "PLAYER_REGEN_DISABLED" then
 	    LastTime = GetTime()
 	elseif event == "PLAYER_REGEN_ENABLED"  then
-		DEFAULT_CHAT_FRAME:AddMessage(format("%s %.2fs", "Last fight's duration:",GetTime()-LastTime))
+		DEFAULT_CHAT_FRAME:AddMessage(format("%s %.2fs", text, GetTime()-LastTime))
 		if SeparateFrame then
-			SeparateFrame:AddMessage(format("%.2f",GetTime()-LastTime))
+			SeparateFrame:AddMessage(format("%.2f", GetTime()-LastTime))
 		end
 	elseif event == "ADDON_LOADED" and addon == "CombatTimer" then
         for i=1,NUM_CHAT_WINDOWS do
